9a7a73a0c34091c371ac4577aa679d0624fdf419
Samina Moin <saminamoinit@gmail.com>
2026-03-25 20:25:42 +0500

    Delete models directory

--- models/hr_employee.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
-
-    x_iqama_id = fields.Char(string='Iqama/ID Number', help="Saudi National ID or Iqama Number")
-    x_bank_iban = fields.Char(string='Bank IBAN', help="Full KSA IBAN starting with SA")
